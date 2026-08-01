c19c4f76580110cd59ac925ce5b276dfcc3bd5ce
Sanket Das <dassanket937@gmail.com>
2026-08-02 02:54:37 +0530

    Initial CRM360 project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>CRM360</h1>
+      <h2>Customer Relationship Management System</h2>
+      <p>Welcome to CRM360!</p>
+    </div>
+  );
+}
+
+export default App;
